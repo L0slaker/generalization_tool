c80d48604c33b2c21a4ce0d18686111484a430d0
L0slaker <116342782+L0slaker@users.noreply.github.com>
2023-08-13 23:07:46 +0800

    Delete generalization_tool/generalization_tool directory

--- generalization_tool/generalization_tool/queue/concurrent_array_blocking_queue.go
+++
@@ -1,5 +0,0 @@
-package queue
-
-// ConcurrentArrayBlockingQueue 有界并发阻塞队列
-type ConcurrentArrayBlockingQueue[T any] struct {
-}
--- generalization_tool/generalization_tool/slice/types.go
+++
@@ -1,4 +0,0 @@
-package slice
-
-// equalFunc 比较两元素是否相等
-type equalFunc[T any] func(src, dst T) bool
